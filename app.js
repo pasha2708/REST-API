527d61fe54627d15b2ae460641bf84c494dc474c
Pavlo Pysaniuk <93548330+pasha2708@users.noreply.github.com>
2022-03-22 17:33:28 +0200

    finish app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import Post from './Post.js'
+import router from './router.js'
+import fileUpload from 'express-fileupload'
 
 const PORT = 5000
 const DB_URL = 'mongodb+srv://user:user1@cluster0.37bro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -8,18 +9,15 @@ const DB_URL = 'mongodb+srv://user:user1@cluster0.37bro.mongodb.net/myFirstDatab
 const app = express()
 
 app.use(express.json())
-
-app.post('/', async (req, res) => {
-  const {author, title, content, picture} = req.body
-  const post = await Post.create({author, title, content, picture})
-  res.json(post)
-})
+app.use(fileUpload())
+app.use(express.static('static'))
+app.use('/api', router)
 
 async function startApp() {
   try {
     await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
     app.listen(PORT, () => {
-      console.log("started")
+      console.log(`started on port ${PORT}`)
     })
   } catch (e) {
     console.log(e);
